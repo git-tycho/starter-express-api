230b2c9e7fe3202bfc73427df850a0c5754d725e
git-tycho <git@bennell.biz>
2023-07-16 14:43:36 -0700

    Update index.js without JSON.parse

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.sendStatus(200)
-    const { event_type, form_response } = JSON.parse(req.body);
+    const form_response = req.body;
     console.log(form_response)
 })
 app.listen(process.env.PORT || 3000)
