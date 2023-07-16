92988c9d6533653c9078a4e702f1b72ef225c736
git-tycho <git@bennell.biz>
2023-07-16 14:31:13 -0700

    Update index.js to log form response

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 app.all('/', (req, res) => {
     console.log("Just got a request!")
-    res.send('Yo!')
+    res.sendStatus(200)
+    const { event_type, form_response } = JSON.parse(request.body);
+    console.log(form_response)
 })
 app.listen(process.env.PORT || 3000)
