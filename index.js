bfbf3a8b65c06183ea8042436effe1b41433646e
git-tycho <git@bennell.biz>
2023-07-16 19:40:45 -0700

    Update index.js with app.post and JSON.parse

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.sendStatus(200)
-    const form_response = req.body;
+app.post('/', async (req, res) => {
+    console.log("Just got a request!");
+    res.sendStatus(200);
+    const { event_type, form_response } = JSON.parse(req.body);
     console.log(form_response)
 })
 app.listen(process.env.PORT || 3000)
